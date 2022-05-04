5208ef540b9edbbd549e68f87408af3f4aac7c92
seleshabani <seleshabani4@gmail.com>
2022-05-04 15:29:54 +0100

    data_for_module(resource)

---
+++ app/Filament/Resources/DataForModuleResource/Pages/EditDataForModule.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\DataForModuleResource\Pages;
+
+use App\Filament\Resources\DataForModuleResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditDataForModule extends EditRecord
+{
+    protected static string $resource = DataForModuleResource::class;
+}
---
+++ app/Filament/Resources/DataForModuleResource/Pages/ListDataForModules.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\DataForModuleResource\Pages;
+
+use App\Filament\Resources\DataForModuleResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListDataForModules extends ListRecords
+{
+    protected static string $resource = DataForModuleResource::class;
+}
